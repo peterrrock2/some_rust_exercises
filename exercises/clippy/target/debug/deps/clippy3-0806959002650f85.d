2b20d13ed209159975ffed6962be9504c9ad6ce7
peterrrock2 <peter.r.rock2@gmail.com>
2023-08-20 23:46:16 -0600

    Removed all of the extra files

---
+++ exercises/clippy/target/debug/deps/clippy3-0806959002650f85.d
@@ -0,0 +1,8 @@
+/home/peter/Dropbox/Coding_Projects/scrap/rustlings/exercises/clippy/target/debug/deps/clippy3-0806959002650f85.rmeta: clippy3.rs Cargo.toml
+
+/home/peter/Dropbox/Coding_Projects/scrap/rustlings/exercises/clippy/target/debug/deps/clippy3-0806959002650f85.d: clippy3.rs Cargo.toml
+
+clippy3.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
